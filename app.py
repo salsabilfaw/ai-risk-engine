6cac5fa326b6e805a7d8fd422102add60d3133de
Salsabil Farah Aqilah Wijaya <salsabilfarahaqilahwijaya@MacBook-Air-Salsabil.local>
2026-04-28 20:23:11 +0700

    init

--- app.py
+++ app.py
@@ -20,18 +20,20 @@ bcrypt = Bcrypt(app)
 import os
 import psycopg2
 
-def get_db():
+ddef get_db():
     db_url = os.getenv("DATABASE_URL")
 
-    if not db_url:
-        raise Exception("DATABASE_URL tidak ditemukan")
+    if db_url:
+        if db_url.startswith("postgres://"):
+            db_url = db_url.replace("postgres://", "postgresql://", 1)
+        return psycopg2.connect(db_url, sslmode="require")
 
-    # fix untuk Render (kadang masih postgres://)
-    if db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
-
-    return psycopg2.connect(db_url, sslmode="require")
-
+    return psycopg2.connect(
+        host="localhost",
+        database="ai_risk_engine",
+        user="postgres",
+        password="password"
+    )
 # ======================
 # MODEL LOAD
 # ======================
